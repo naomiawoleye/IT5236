559c8f1d2260f26f3a183d7b929e8f574963fa95
naomiawoleye <46787114+naomiawoleye@users.noreply.github.com>
2019-10-23 17:52:42 -0400

    Update tasks.php

--- api/tasks.php
+++ api/tasks.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 			$stmt = $dbh->prepare($sql);
 			$stmt->execute();
 			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			http_response_code(201); //task created
+			http_response_code(200); //task created
 			echo json_encode ($result);
 			exit();
 			
